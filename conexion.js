e7f2faca2b85d00f809e859c8d3ed5942fe194dd
LucioHdz <luciohdz0@gmail.com>
2024-05-30 08:46:45 -0600

    v1

---
+++ conexion.js
@@ -0,0 +1,72 @@
+async function fetchUsuarios() {
+    const response = await fetch('http://localhost:3000/');
+    const usuarios = await response.json();
+    const listaRegistros = document.getElementById('listaRegistros');
+    listaRegistros.innerHTML = '';
+    usuarios.forEach(usuario => {
+        const li = document.createElement('li');
+        li.className = 'list-group-item d-flex justify-content-between align-items-center';
+        li.textContent = `${usuario.id}: ${usuario.nombre}`;
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = 'Eliminar';
+        deleteBtn.className = 'btn btn-danger btn-sm';
+        deleteBtn.addEventListener('click', async () => {
+            try {
+                const response = await fetch('http://localhost:3000/', {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ id: usuario.id })
+                });
+                if (response.ok) {
+                    alert('Usuario eliminado');
+                    li.remove();
+                } else {
+                    const errorData = await response.json();
+                    alert('Error al eliminar el usuario: ' + errorData.message);
+                }
+            } catch (error) {
+                console.error('Error al eliminar el usuario:', error);
+                alert('Error al eliminar el usuario');
+            }
+        });
+        li.appendChild(deleteBtn);
+        listaRegistros.appendChild(li);
+    });
+}
+
+document.getElementById('agregarBtn').addEventListener('click', async () => {
+    
+    
+    const nombre = document.getElementById('nombre').value;
+    const response = await fetch('http://localhost:3000/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ id: 0, nombre })
+    });
+    const result = await response.json();
+    if (response.ok) {
+        Swal.fire({
+            icon: "success",
+            title: "Usuario Agregado",
+            text:"se agrego a la base de datos",
+            showConfirmButton: true,
+            showCancelButton: true,
+            cancelButtonText: "No permitir",
+            cancelButtonColor:"#FF6663",
+            confirmButtonText:"Enterado",
+            footer:"<h1>mensaje</h1>",
+            theme:"dark"
+        });
+        document.getElementById('id').value = '';
+        document.getElementById('nombre').value = '';
+        fetchUsuarios();
+    } else {
+        alert('Error al agregar el usuario: ' + result.message);
+    }
+});
+
+document.getElementById('verUsuariosBtn').addEventListener('click', fetchUsuarios);
